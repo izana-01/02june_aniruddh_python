f467d9e1d5781547c963fcae1113845a8e1bcd44
izana-01 <aniruddhdabhi5@gmail.com>
2025-06-06 04:36:51 +0530

    first commit

---
+++ assignments/module-1/print.py
@@ -0,0 +1,7 @@
+print("hy")
+print('hello')
+print("dabhi aniruddhsinh")
+print("""29 july 2005""") 
+print('rajkot')
+print("""gondal
+      rajkot""")
---
+++ assignments/module-1/var.py
@@ -0,0 +1,29 @@
+a=100
+b=119
+
+print ("sum of a+b is=", a+b )
+print ("a/b is=", a/b)
+print ("a*b=", a*b)
+print ("a-b is=", a-b)
+
+
+#this is for example
+
+"""a=100
+b=119
+
+print ("sum of a+b is=", a+b )
+print ("a/b is=", a/b)
+print ("a*b=", a*b)
+print ("a-b is=", a-b) 
+"""
+
+
+'''a=100
+b=119
+
+print ("sum of a+b is=", a+b )
+print ("a/b is=", a/b)
+print ("a*b=", a*b)
+print ("a-b is=", a-b)'''
+
